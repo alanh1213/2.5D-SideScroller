f81cf3422a6c0b0c75013dd8a852da03f7aa4d8c
Alan <alanh1213@hotmail.com>
2022-03-10 21:37:49 -0300

    Suba de proyecto

---
+++ ZombieSideScroller 25D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Animator playerAnim;
+    Rigidbody rb2d;
+    [SerializeField] float runSpeed;
+    [SerializeField] float walkSpeed;
+    bool facingRight;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerAnim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody>();
+    }
+
+    private void Start() {
+        facingRight = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        playerAnim.SetFloat("speed", Mathf.Abs(move));
+        float sneaking = Input.GetAxisRaw("Fire3");
+        playerAnim.SetFloat("sneaking", sneaking);
+
+        if(sneaking > 0){
+            rb2d.velocity = new Vector3(move * walkSpeed, rb2d.velocity.y, 0);
+        }else{
+            rb2d.velocity = new Vector3(move * runSpeed, rb2d.velocity.y, 0);
+        }
+        
+
+        if(move > 0 && !facingRight)Flip();
+        else if(move < 0 && facingRight) Flip();
+    }
+
+    void Flip(){
+        facingRight = !facingRight;
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z * -1);
+    }
+}
